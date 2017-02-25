b8b0380132fcf0fe4a495eab9bb82fa97ed77929
Dhruv kumar <darbaga10@gmail.com>
2017-02-25 19:06:49 +0530

    Add loading/unloading and a test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+extern crate tolk_sys;
+
+use tolk_sys::*;
+
+struct Tolk;
+
+impl Tolk {
+    pub fn new() -> Tolk {
+        unsafe { Tolk_Load(); }
+        Tolk
     }
+}
+
+impl Drop for Tolk {
+    fn drop(&mut self) {
+        unsafe { Tolk_Unload(); }
+    }
+}
+
+#[test]
+fn test_drop_behavior() {
+    unsafe {
+        let f = Tolk::new();
+        assert_eq!(Tolk_IsLoaded(), true);
+    }
+
+    unsafe { assert_eq!(Tolk_IsLoaded(), false); }
 }
