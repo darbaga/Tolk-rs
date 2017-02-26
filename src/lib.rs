cf518ee6c1d61bf80f504693170b59b037508acc
Dhruv kumar <darbaga10@gmail.com>
2017-02-26 17:28:21 +0530

    Add output and detect screen reader

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 extern crate tolk_sys;
 
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+use std::slice::from_raw_parts;
+
 use tolk_sys::*;
 
 pub struct Tolk;
@@ -9,12 +13,32 @@ impl Tolk {
         unsafe { Tolk_Load(); }
         Tolk
     }
+
+    pub fn detect_screen_reader(&self) -> String {
+        unsafe { return string_from_wchar_t(Tolk_DetectScreenReader());}
+    }
+
+    pub fn output(&self, s: &str) {
+        let widetext = OsStr::new(s).encode_wide().chain(Some(0).into_iter()).collect::<Vec<_>>().as_ptr();
+        unsafe { Tolk_Output(widetext, true)};
+    }
 }
 
 impl Drop for Tolk {
     fn drop(&mut self) {
         unsafe { Tolk_Unload(); }
     }
+}
+
+unsafe fn string_from_wchar_t(orig: *const u16) -> String {
+    let mut len = 0;
+
+    while *orig.offset(len) != 0 {
+        len += 1;
+    }
+
+    let sl = from_raw_parts(orig, len as usize);
+    String::from_utf16(sl).unwrap()
 }
 
 #[test]
